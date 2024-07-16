eb4345917e3892c38ab82e4ce446fbbeb1b83ddf
Tim <tim.matzenauer@outlook.de>
2024-07-16 18:59:30 +0200

    Delete File used for Development process

--- Private/ps1/PrivateVariables.ps1
+++
@@ -1,4 +0,0 @@
-$ApiKey = "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
-$ApiKey2 =  "cac4ba5880abc5f0cbab8ea3cf3bd6ba35a97bc9"
-$orgId = "646829496481090341" #SCCTest
-$ApiBaseUrl = "https://api.meraki.com/api/v1"
